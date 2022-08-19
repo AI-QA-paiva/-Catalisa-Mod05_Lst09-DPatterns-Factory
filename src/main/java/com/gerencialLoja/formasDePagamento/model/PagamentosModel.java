b7ab8c55928ff5711e714d0d54514e00c7d615e8
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-08-19 14:03:38 -0300

    Primeiro etapa da atividade, criar função de pagamentos, inserido com sucesso, versionar remoto

---
+++ src/main/java/com/gerencialLoja/formasDePagamento/model/PagamentosModel.java
@@ -0,0 +1,38 @@
+package com.gerencialLoja.formasDePagamento.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Data
+@Entity
+@Getter
+@NoArgsConstructor
+@Setter
+@Table(name = "pagamento")
+public class PagamentosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(length = 50, nullable = false)
+    private String status;
+
+//    @Column(length = 50, nullable = false)
+//    private String metodoPgo;
+
+    @Column(length = 10, nullable = false)
+    private BigDecimal valorAPagar;
+
+    @Column(length = 10, nullable = false)
+    private BigDecimal diferencaValor;
+
+    @Column(length = 10, nullable = false)
+    private BigDecimal valorPago;
+
+//    @Column(length = 10, nullable = false)
+//    private BigDecimal valorRecebido;
+
+}
